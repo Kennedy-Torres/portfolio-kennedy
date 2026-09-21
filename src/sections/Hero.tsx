9f37133ddc2ae4e1ee960df1d57bdf44afa60912
Kennedy <krtorres.programador@hotmail.com>
2026-09-20 23:50:13 -0300

    fix: Correção da referência de diretório p/ CV

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -64,8 +64,8 @@ export function Hero() {
             LinkedIn
           </a>
           <a
-            href="/cv-kennedy-torres.pdf"
-            download="cv-kennedy-torres.pdf"
+            href="../portfolio-kennedy/public/cv-kennedy-torres.pdf"
+            download
             target="_blank"
             rel="noopener noreferrer"
             className="bg-white border border-gray-300 text-gray-800 dark:bg-[#111c30] dark:border-gray-700 dark:text-gray-200 dark:hover:bg-[#182642] px-6 py-3 rounded-lg font-medium hover:bg-gray-100 transition-colors shadow-sm flex items-center gap-2"
