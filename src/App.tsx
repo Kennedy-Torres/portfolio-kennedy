b4f39f08bd56f0f61a652f2c143131158d58c987
Kennedy <krtorres.programador@hotmail.com>
2026-09-23 10:34:28 -0300

    fix: resolve navegação por link âncora no primeiro carregamento da página

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,29 @@ import { About } from './sections/About';
 import { Skills } from './sections/Skills';
 import { Projects } from './sections/Projects';
 import { Footer } from './components/Footer';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    const hash = window.location.hash;
+
+    if (hash) {
+      // O setTimeout garante que o React terminou de desenhar o HTML na tela
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100); // pequeno atraso de 100 milissegundos para dar tempo do React injetar todo o seu código na tela e, em seguida, simula o comportamento nativo do navegador, rolando suavemente até a seção exata que foi compartilhada
+    }
+  }, []);
+
+
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
-      
+
       <main className="flex flex-col items-center w-full flex-grow">
         <Hero />
         <About />
